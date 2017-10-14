a2747bd3e042f44a748405be72e5a95f7c61ac0e
Lewis Saunders <lewis.saunders@gmail.com>
2017-10-14 19:54:52 +0100

    709 ycbcr to rgb the long and upside-down way

--- Ls_Spigot.cpp
+++ Ls_Spigot.cpp
@@ -62,29 +62,73 @@ unsigned long *SparkProcess(SparkInfoStruct si) {
 	clock_gettime(CLOCK_REALTIME, &s);
 	float ms = (s.tv_nsec - last.tv_nsec) / 1000000.0;
 	cout << ms << "ms since last process call" << endl;
-	
+
 	SparkMemBufStruct buf;
 	sparkBuf(1, &buf);
 
-	uint16_t *rgb = (uint16_t *)buf.Buffer;
-	uint32_t *v210 = (uint32_t *)fbuf;
+	half *rgb = (half *)buf.Buffer;
+	int *v210 = (int *)fbuf;
 
+	/* TODO:
+		flip
+		double buffering
+		re-entrancy
+		chroma interpolation
+		speed
+		safety
+	*/
+	while(rgb < (half *)buf.Buffer + (1920 * 1080 * 3)) {
+		float y0 = (v210[0] >> 10) & 0x000003ff;
+		float y1 = (v210[1] >>  0) & 0x000003ff;
+		float y2 = (v210[1] >> 20) & 0x000003ff;
+		float y3 = (v210[2] >> 10) & 0x000003ff;
+		float y4 = (v210[3] >>  0) & 0x000003ff;
+		float y5 = (v210[3] >> 20) & 0x000003ff;
+		float cr0 = (v210[0] >> 20) & 0x000003ff;
+		float cr2 = (v210[2] >>  0) & 0x000003ff;
+		float cr4 = (v210[3] >> 10) & 0x000003ff;
+		float cb0 = (v210[0] >> 00) & 0x000003ff;
+		float cb2 = (v210[1] >> 10) & 0x000003ff;
+		float cb4 = (v210[2] >> 20) & 0x000003ff;
 
-	while(rgb < (uint16_t *)buf.Buffer + (1920 * 1080 * 3)) {
-		uint16_t y0 = (v210[0] >> 10) & 0x000003ff;
-		uint16_t y1 = (v210[1] >>  0) & 0x000003ff;
-		uint16_t y2 = (v210[1] >> 20) & 0x000003ff;
-		uint16_t y3 = (v210[2] >> 10) & 0x000003ff;
-		uint16_t y4 = (v210[3] >>  0) & 0x000003ff;
-		uint16_t y5 = (v210[3] >> 20) & 0x000003ff;
+		y0 = y0 - 64.0;
+		y1 = y1 - 64.0;
+		y2 = y2 - 64.0;
+		y3 = y3 - 64.0;
+		y4 = y4 - 64.0;
+		y5 = y5 - 64.0;
+		cr0 = cr0 - 512.0;
+		cr2 = cr2 - 512.0;
+		cr4 = cr4 - 512.0;
+		cb0 = cb0 - 512.0;
+		cb2 = cb2 - 512.0;
+		cb4 = cb4 - 512.0;
+
+		rgb[0] = (y0 * 1.164 + cb0 *  0.000 + cr0 *  1.793) / 1023.0;
+		rgb[1] = (y0 * 1.164 + cb0 * -0.213 + cr0 * -0.533) / 1023.0;
+		rgb[2] = (y0 * 1.164 + cb0 *  2.112 + cr0 *  0.000) / 1023.0;
+
+		rgb[3] = (y1 * 1.164 + cb0 *  0.000 + cr0 *  1.793) / 1023.0;
+		rgb[4] = (y1 * 1.164 + cb0 * -0.213 + cr0 * -0.533) / 1023.0;
+		rgb[5] = (y1 * 1.164 + cb0 *  2.112 + cr0 *  0.000) / 1023.0;
+
+		rgb[6] = (y2 * 1.164 + cb2 *  0.000 + cr2 *  1.793) / 1023.0;
+		rgb[7] = (y2 * 1.164 + cb2 * -0.213 + cr2 * -0.533) / 1023.0;
+		rgb[8] = (y2 * 1.164 + cb2 *  2.112 + cr2 *  0.000) / 1023.0;
+
+		rgb[9]  = (y3 * 1.164 + cb2 *  0.000 + cr2 *  1.793) / 1023.0;
+		rgb[10] = (y3 * 1.164 + cb2 * -0.213 + cr2 * -0.533) / 1023.0;
+		rgb[11] = (y3 * 1.164 + cb2 *  2.112 + cr2 *  0.000) / 1023.0;
+
+		rgb[12] = (y4 * 1.164 + cb4 *  0.000 + cr4 *  1.793) / 1023.0;
+		rgb[13] = (y4 * 1.164 + cb4 * -0.213 + cr4 * -0.533) / 1023.0;
+		rgb[14] = (y4 * 1.164 + cb4 *  2.112 + cr4 *  0.000) / 1023.0;
+
+		rgb[15] = (y5 * 1.164 + cb4 *  0.000 + cr4 *  1.793) / 1023.0;
+		rgb[16] = (y5 * 1.164 + cb4 * -0.213 + cr4 * -0.533) / 1023.0;
+		rgb[17] = (y5 * 1.164 + cb4 *  2.112 + cr4 *  0.000) / 1023.0;
+
 		v210 += 4;
-
-		rgb[0] = y0 << 4;
-		rgb[3] = y1 << 4;
-		rgb[6] = y2 << 4;
-		rgb[9] = y3 << 4;
-		rgb[12] = y4 << 4;
-		rgb[15] = y5 << 4;
 		rgb += 6 * 3;
 	}
 
